c927dafc30e250362f8827a79fca278029b13086
Muhammad Brilian Erranaomi <49330514+brili99@users.noreply.github.com>
2024-12-16 14:38:31 +0700

    Create TimerNonBlocking.h

---
+++ TimerNonBlocking.h
@@ -0,0 +1,14 @@
+#ifndef _TIMER_NON_BLOCKING_H_
+#define _TIMER_NON_BLOCKING_H_
+
+class TimerNonBlocking
+{
+public:
+    TimerNonBlocking(unsigned long interval);
+    unsigned long last_ts;
+    unsigned long interval_loop;
+    void set_interval(unsigned long interval);
+    bool loop(unsigned long &current_timestamp, bool reset = true);
+};
+
+#endif
